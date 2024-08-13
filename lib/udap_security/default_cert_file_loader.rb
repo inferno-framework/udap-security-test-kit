e6f902feb17c605e698a2707d589859e4cee0afa
Alisa Wallace <awallace@mitre.org>
2024-08-13 08:23:51 -0700

    Make cert filepath references consistent

--- lib/udap_security/default_cert_file_loader.rb
+++ lib/udap_security/default_cert_file_loader.rb
@@ -13,13 +13,13 @@ module UDAPSecurity
     end
 
     def self.load_test_client_cert_pem_file
-      raw_cert = File.read(File.join(File.dirname(__FILE__), '../../lib/udap_security/certs/TestClient.pem'))
+      raw_cert = File.read(File.join(File.dirname(__FILE__), 'certs/TestClient.pem'))
       cert = OpenSSL::X509::Certificate.new raw_cert
       cert.to_pem
     end
 
     def self.load_test_client_private_key_file
-      raw_key = File.read(File.join(File.dirname(__FILE__), '../../lib/udap_security/certs/TestClientPrivateKey.key'))
+      raw_key = File.read(File.join(File.dirname(__FILE__), 'certs/TestClientPrivateKey.key'))
       key = OpenSSL::PKey::RSA.new raw_key
       key.to_pem
     end

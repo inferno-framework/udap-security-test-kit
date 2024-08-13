fb634c3e03efaf3c94128d54b797f54cad1c8738
Alisa Wallace <awallace@mitre.org>
2024-08-13 08:22:21 -0700

    Revert "Updated default cert file loader to point to spec/fixtures"
    This reverts commit 869c0610370f37573a2ff7a8e559f068e9d557eb.

--- lib/udap_security/default_cert_file_loader.rb
+++ lib/udap_security/default_cert_file_loader.rb
@@ -1,25 +1,25 @@
 module UDAPSecurity
   class DefaultCertFileLoader
     def self.load_default_ca_pem_file
-      raw_cert = File.read(File.join(File.dirname(__FILE__), '../../spec/fixtures/InfernoCA.pem'))
+      raw_cert = File.read(File.join(File.dirname(__FILE__), 'certs/InfernoCA.pem'))
       cert = OpenSSL::X509::Certificate.new raw_cert
       cert.to_pem
     end
 
     def self.load_default_ca_private_key_file
-      raw_key = File.read(File.join(File.dirname(__FILE__), '../../spec/fixtures/InfernoCA.key'))
+      raw_key = File.read(File.join(File.dirname(__FILE__), 'certs/InfernoCA.key'))
       private_key = OpenSSL::PKey::RSA.new raw_key
       private_key.to_pem
     end
 
     def self.load_test_client_cert_pem_file
-      raw_cert = File.read(File.join(File.dirname(__FILE__), '../../spec/fixtures/TestClient.pem'))
+      raw_cert = File.read(File.join(File.dirname(__FILE__), '../../lib/udap_security/certs/TestClient.pem'))
       cert = OpenSSL::X509::Certificate.new raw_cert
       cert.to_pem
     end
 
     def self.load_test_client_private_key_file
-      raw_key = File.read(File.join(File.dirname(__FILE__), '../../spec/fixtures/TestClientPrivateKey.key'))
+      raw_key = File.read(File.join(File.dirname(__FILE__), '../../lib/udap_security/certs/TestClientPrivateKey.key'))
       key = OpenSSL::PKey::RSA.new raw_key
       key.to_pem
     end

ec8e92059977cfce9dd59cc0e9921c63f1e0b2b2
Alisa Wallace <awallace@mitre.org>
2025-05-21 16:28:58 -0700

    Fix requirement 223

--- lib/udap_security_test_kit/client_suite/token_request_cc_verification_test.rb
+++ lib/udap_security_test_kit/client_suite/token_request_cc_verification_test.rb
@@ -48,6 +48,7 @@ module UDAPSecurityTestKit
                           'hl7.fhir.us.udap-security_1.0.0_reqs@212',
                           'hl7.fhir.us.udap-security_1.0.0_reqs@214',
                           'hl7.fhir.us.udap-security_1.0.0_reqs@215',
+                          'hl7.fhir.us.udap-security_1.0.0_reqs@223',
                           'hl7.fhir.us.udap-security_1.0.0_reqs@225',
                           'hl7.fhir.us.udap-security_1.0.0_reqs@226',
                           'hl7.fhir.us.udap-security_1.0.0_reqs@227',
